88f26eac67f46266bac889d0240afad46263d197
Neha-Patel13 <122043318+Neha-Patel13@users.noreply.github.com>
2023-02-19 13:44:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ const [expense, setExpense] = useState(0);
   return (
     <div id="main">
       <input id="expense-input" />
-      <button id="expense-button">Add Expense</button>
+      <button id="expense-button" onClick={handle}>Add Expense</button>
       <div id="expense-list">
       </div>
       <div id="total-expense">
